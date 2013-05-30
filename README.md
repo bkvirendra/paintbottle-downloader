002912de0637940e18a444e82786fb5f545e03fb
Virendra Rajput <virendra.rajput567@gmail.com>
2013-05-31 03:47:23 +0530

    published

---
+++ README.md
@@ -0,0 +1,44 @@
+paintbottle-downloader
+======================
+
+Downloads all the videos from http://paintbottle.com
+
+Requirements:
+
+    requests==0.14.2
+    python-progressbar==2.3
+
+Installing requirements
+-----------------------
+
+####Installing requests:
+
+    $ pip install requests==0.14.2
+    
+####Installing python-progressbar:
+
+    $ wget https://python-progressbar.googlecode.com/files/progressbar-2.3.tar.gz
+    $ tar xzvf progressbar-2.3.tar.gz
+    $ cd progressbar-2.3/
+    $ sudo python setup.py install
+    
+Usage:
+-----
+
+    $ python paint.py
+    
+#####this will start downloading all the videos from [paintbottle.com](http://paintbottle.com) one-by-one, make sure you have enough disk space, and a good internet speed. 
+
+
+about me:
+----------
+
+You can follow me on Twitter [@bkvirendra](http://twitter.com/bkvirendra)
+
+I blog at http://virendra.me
+
+License:
+--------
+
+I dont care about the any licenses, its all yours :)
+
---
+++ paint.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+__author__ = "Virendra Rajput"
+
+import os
+import sys
+import gc
+import urllib
+import requests
+
+from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar, RotatingMarker
+
+directory = "paintbottlevideos/"
+
+widgets = ['downloading..: ', Percentage(), ' ', Bar(marker=RotatingMarker()), ' ', ETA(), ' ', FileTransferSpeed()]
+pbar = ProgressBar(widgets=widgets)
+
+def dlProgress(count, blockSize, totalSize):
+    if pbar.maxval is None:
+        pbar.maxval = totalSize
+        pbar.start()
+    pbar.update(min(count*blockSize, totalSize))
+
+def main():
+ 		print "fetching video list from paintbottle.com.."
+        data = requests.get("http://paintbottle.com/videos/straight.json").json
+        count = 1
+        if not os.path.exists(directory):
+            print "paintbottlevideos directory created!"
+            os.makedirs(directory)
+        for item in data[0].get("videos"):
+                if item.get("src"):
+                        # download the item
+                        if not os.path.isfile("paintbottlevideos/" + item.get("src") + ".mp4"):
+                                url = "http://paintbottlevideos.com/" + item.get("src") + ".mp4"
+                                print "Started downloading.. item " + str(count) + " of " + str(len(data[0].get("videos")))
+                                try:
+                                        urllib.urlretrieve(url, "paintbottlevideos/" + url.split("/")[-1], reporthook=dlProgress)
+                                except Exception, e:
+                                        main()
+                                print "Finsished downloading item.. " + str(count)
+                                gc.collect()
+                        count += 1
+        print "Downloading complete..\nEnjoy the show ;)"
+        return
+
+if __name__ == '__main__':
+        main()
